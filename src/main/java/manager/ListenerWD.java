222223392a47b50573a2604a8dad619092401120
yuliyapokh <pokh.yul@gmail.com>
2023-01-31 22:41:20 +0200

    ggg

--- src/main/java/manager/ListenerWD.java
+++ src/main/java/manager/ListenerWD.java
@@ -20,57 +20,55 @@ public class ListenerWD implements WebDriverListener {
     @Override
     public void onError(Object target, Method method, Object[] args, InvocationTargetException e) {
         WebDriverListener.super.onError(target, method, args, e);
-        logger.info("We have a 'PROBLEM'");
-        logger.info("******************************");
-        logger.info("Method method -->" + method.getName());
-        logger.info("******************************");
-        //logger.info("InvocationTargetException getMessage" +e.getMessage()); //null
-
-        logger.info("InvocationTargetException getTargetException" + e.getTargetException());
-        logger.info("******************************");
-        //logger.info("InvocationTargetException getCause" +e.getCause());
-
-        logger.info("Object target---->> " + target.toString());
-        logger.info("******************************");
-        WebDriver wd = (ChromeDriver) target;
-
+        logger.info("The have a 'problem'");
+        logger.info("******************");
+        logger.info("Method method ---> " +method.getName());
+        logger.info("******************");
+        // logger.info("InvocationTargetException getMessage()" +e.getMessage()); /// null
+        logger.info("InvocationTargetException getTargetException()" +e.getTargetException());
+        logger.info("******************");
+//        logger.info("InvocationTargetException Cause()" +e.getCause());
+//        logger.info("******************");
+        logger.info("Object target --->" +target.toString());
+        logger.info("******************");
+        WebDriver wd = (ChromeDriver)target;
         int i = new Random().nextInt(1000);
         String link = "src/test/screenshots/screen-"+i+".png";
         logger.info("Screen with error is --->" +link);
 
         //TakesScreenshot takesScreenshot = (TakesScreenshot) wd;
-       // takesScreenshot.getScreenshotAs(OutputType.FILE);
+        // takesScreenshot.getScreenshotAs(OutputType.FILE);
         File tmp =  ((TakesScreenshot) wd).getScreenshotAs(OutputType.FILE);
         try {
             Files.copy(tmp,new File(link));
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
-
     }
 
     @Override
     public void beforeFindElement(WebDriver driver, By locator) {
         WebDriverListener.super.beforeFindElement(driver, locator);
-        logger.info("Before find element --> " + locator);
+        logger.info("Before find element --> " +locator);
+
     }
 
     @Override
     public void afterFindElement(WebDriver driver, By locator, WebElement result) {
         WebDriverListener.super.afterFindElement(driver, locator, result);
-        logger.info("After find element --> " + locator);
+        logger.info("After find element --> " +locator);
     }
 
     @Override
     public void beforeFindElements(WebDriver driver, By locator) {
         WebDriverListener.super.beforeFindElements(driver, locator);
-        logger.info("Before find elements --> " + locator);
+        logger.info("Before find elements --> " +locator);
     }
 
     @Override
     public void afterFindElements(WebDriver driver, By locator, List<WebElement> result) {
         WebDriverListener.super.afterFindElements(driver, locator, result);
-        logger.info("After find elements --> " + locator);
-        logger.info("List size is --->" + result.size());
+        logger.info("After find elements --> " +locator);
+        logger.info("List suze is --> " +result.size());
     }
 }
